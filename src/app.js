1ae1b68dc868faf7dbc2e2825a68926e9e7188ad
Maoris R <FMRN01201@COMMUN.AD.SNCF.FR>
2020-05-04 10:44:30 +0200

    route end point

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import bodyParser  from "body-parser";
 import cors from "cors";
 
+import prdRoutes from "./routes/index";
 const app = express();
 
 app.use(cors());
@@ -10,4 +11,6 @@ app.use(morgan("dev"));
 // Body Parser configuration
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/api/", prdRoutes);
 export default app;
